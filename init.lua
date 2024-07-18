fb9e9cf67ac542bfc8effadea4229999c8736547
m51k <mekail@gmail.com>
2024-07-18 21:54:36 +0500

    initial commit
    where there was darkness, there is now light

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("config.lazy")
+vim.cmd[[colorscheme solarized-osaka]]
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,55 @@
+return {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require('telescope').setup({
+            path_display = { "smart" },
+            defaults = {
+                -- Default configuration for telescope goes here:
+                -- config_key = value,
+                mappings = {
+                    i = {
+                        -- map actions.which_key to <C-h> (default: <C-/>)
+                        -- actions.which_key shows the mappings for your picker,
+                        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                    },
+                },
+            },
+            pickers = {
+                -- Default configuration for builtin pickers goes here:
+                -- picker_name = {
+                --   picker_config_key = value,
+                --   ...
+                -- }
+                -- Now the picker_config_key will be applied every time you call this
+                -- builtin picker
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,                    -- false will only do exact matching
+                    override_generic_sorter = true,  -- override the generic sorter
+                    override_file_sorter = true,     -- override the file sorter
+                    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+                -- Your extension configuration goes here:
+                -- extension_name = {
+                --   extension_config_key = value,
+                -- }
+                -- please take a look at the readme of the extension you want to configure
+            },
+        })
+
+        require('telescope').load_extension('fzf')
+
+        vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
+        vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+        vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string" })
+        vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor" })
+        vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffer" })
+    end,
+}
