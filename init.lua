096129a73989fe0f64df612966069d9d6ffacd59
m51k <mekail@gmail.com>
2025-07-29 01:48:39 +0530

    update

--- init.lua
+++ init.lua
@@ -15,3 +15,54 @@ if not vim.loop.fs_stat(mini_path) then
 end
 
 require("mini.deps").setup({ path = { package = path_package } })
+
+local add = MiniDeps.add
+
+add({ source = "ibhagwan/fzf-lua" })
+add({ source = "bluz71/vim-moonfly-colors" })
+add({ source = "neovim/nvim-lspconfig" })
+add({
+    source = "nvim-treesitter/nvim-treesitter",
+    checkout = "master",
+    monitor = "main",
+    hooks = { post_checkout = function() vim.cmd("TSUpdate") end },
+})
+add({
+    source = "mfussenegger/nvim-dap",
+    depends = { "igorlfs/nvim-dap-view", "theHamsta/nvim-dap-virtual-text" },
+})
+
+vim.o.guicursor = ""
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.g.mapleader = vim.keycode("<Space>")
+vim.cmd("colorscheme moonfly")
+
+vim.lsp.enable({
+    "lua_ls",
+    "clangd",
+})
+vim.diagnostic.config({ virtual_text = true })
+
+require("nvim-treesitter.configs").setup({
+    highlight = { enable = true },
+})
+require("fzf-lua").setup({
+    fzf_colors = { true },
+})
+
+local dap = require("dap")
+local dv = require("dap-view")
+
+dap.listeners.before.attach["dap-view-config"] = function() dv.open() end
+dap.listeners.before.launch["dap-view-config"] = function() dv.open() end
+dap.listeners.before.event_terminated["dap-view-config"] = function() dv.close(true) end
+dap.listeners.before.event_exited["dap-view-config"] = function() dv.close(true) end
+-- TODO: nvim-dap config
+
+-- fzf-lua keys
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
+
+-- nvim-dap keys
+-- TODO
